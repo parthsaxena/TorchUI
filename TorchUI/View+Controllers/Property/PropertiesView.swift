aa5c69137928a1834b757da241c257a064a18d55
Mubashir Mushir <mubashirmushir@outlook.com>
2023-11-24 23:33:04 +0500

    add more files

--- TorchUI/View+Controllers/Property/PropertiesView.swift
+++ TorchUI/View+Controllers/Property/PropertiesView.swift
@@ -11,7 +11,6 @@ import SwiftUI_Shimmer
 struct PropertiesView: View {
     @Environment(\.colorScheme) var colorScheme
     @State private var showingSheet = false
-//    @State private var showSplashScreen = true
     
     let tabs: [TabBarItemData] = [
         TabBarItemData(image: "home", selectedImage: "home", size: 24),
@@ -19,12 +18,6 @@ struct PropertiesView: View {
         TabBarItemData(image: "main-nav", selectedImage: "roster-icon", size: 54),
         TabBarItemData(image: "notif", selectedImage: "match-icon", size: 24),
         TabBarItemData(image: "settings", selectedImage: "match-icon", size: 24)]
-//    let properties: [Property] = [
-//        Property(id: 0, propertyName: "House in Napa", propertyAddress: "2237 Kamp Court", propertyImage: "Property"),
-//        Property(id: 1, propertyName: "House in Napa", propertyAddress: "2237 Kamp Court", propertyImage: "Property"),
-//        Property(id: 2, propertyName: "House in Napa", propertyAddress: "2237 Kamp Court", propertyImage: "Property"),
-//        Property(id: 3, propertyName: "House in Napa", propertyAddress: "2237 Kamp Court", propertyImage: "Property"),
-//    ]        
     
     private let width = UIScreen.main.bounds.width
     private let height = UIScreen.main.bounds.height
@@ -33,12 +26,11 @@ struct PropertiesView: View {
     
     var body: some View {
         ZStack {
-            // List alerts / properties
+
             if SessionManager.shared.showSplashScreen {
                 LoadingSplashScreen()
                     .onAppear(perform: hideSplashScreen)
             } else if !SessionManager.shared.propertiesLoaded || SessionManager.shared.unparsedProperties > 0 {
-//                let x = // print("shimmer \(SessionManager.shared.propertiesLoaded) \(SessionManager.shared.unparsedProperties > 0)")
                 LoadingPropertiesView(showingSheet: $showingSheet)
                 VStack {
                     HeadingView()
@@ -82,7 +74,6 @@ struct PropertiesView: View {
     
     func hideSplashScreen() {
         DispatchQueue.main.asyncAfter(deadline: .now() + 2) {
-            // print("OVER")
             SessionManager.shared.showSplashScreen = false
         }
     }
--- TorchUI/View+Controllers/Property/SearchResultView.swift
+++ TorchUI/View+Controllers/Property/SearchResultView.swift
@@ -13,6 +13,7 @@ struct SearchResultView: View {
     var userEntry: String
     
     private struct HighlightedText: View {
+        
         var text: String
         var highlighted: String
 
@@ -21,25 +22,25 @@ struct SearchResultView: View {
         }
         
         func findLengthOfCommonPrefix(str1: String, str2: String) -> Int {
-            for (idx, c) in str1.enumerated() {
+            for (idx, _) in str1.enumerated() {
                 if idx >= str2.count || str1[idx].lowercased() != str2[idx].lowercased() {
                     return idx
                 }
             }
-            
             return 0
         }
 
         private var attributedString: AttributedString {
+            
             var attributedString = AttributedString(text)
             let prefixIdx = findLengthOfCommonPrefix(str1: text.lowercased(), str2: highlighted.lowercased())
-//            // print("high: \(highlighted) : text: \(text) : idx: \(prefixIdx)")
-            let h = highlighted.substring(to: highlighted.firstIndex(of: highlighted[min(prefixIdx, highlighted.count - 1)])!)
-
-            if let range = AttributedString(text.lowercased()).range(of: h.lowercased()) {
-                attributedString[range].backgroundColor = Color(red: 0.18, green: 0.21, blue: 0.22).opacity(0.1)
+            let highLight = highlighted[min(prefixIdx, highlighted.count - 1)]
+            if let firstIndex = highlighted.firstIndex(of: highLight) {
+                let h = highlighted.substring(to: firstIndex)
+                if let range = AttributedString(text.lowercased()).range(of: h.lowercased()) {
+                    attributedString[range].backgroundColor = Color(red: 0.18, green: 0.21, blue: 0.22).opacity(0.1)
+                }
             }
-
             return attributedString
         }
     }
@@ -49,23 +50,10 @@ struct SearchResultView: View {
             Image("LocationMarker")
                 .resizable()
                 .frame(width: 16, height: 16)
-            
-//            let addressArray = Array(address)
-//            let prefixIdx = findLengthOfCommonPrefix(str1: address, str2: userEntry)
-            
             HighlightedText(text: address, highlighted: userEntry)
                 .font(Font.custom("Manrope-Medium", fixedSize: 14.0))
-            
-//            Text(address[0..<prefixIdx])
-//                .font(Font.custom("Manrope-Medium", fixedSize: 14.0))
-//                .background(Color(red: 0.18, green: 0.21, blue: 0.22))
-//
-//            +            Text(address[prefixIdx...])
-//                .font(Font.custom("Manrope-Medium", fixedSize: 14.0))
-            
             Spacer()
         }
-//        .padding(.horizontal, 2)
         .padding(.vertical, 8)
     }
 }
