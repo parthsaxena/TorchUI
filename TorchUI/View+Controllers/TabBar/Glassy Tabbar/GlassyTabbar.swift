214b0efcc8e79b45787f98b568d3e8c6b6ea0f20
itachi <itachi4760@gmail.com>
2023-12-03 06:36:07 +0400

    tabbar completed

--- TorchUI/View+Controllers/TabBar/Glassy Tabbar/GlassyTabbar.swift
+++ TorchUI/View+Controllers/TabBar/Glassy Tabbar/GlassyTabbar.swift
@@ -80,3 +80,16 @@ struct CustomTabBar: View {
 #Preview {
     CustomTabBar(selectedTab: .constant(.home))
 }
+
+struct GlassyEffectView: UIViewRepresentable {
+    var effect: UIVisualEffect?
+    var cornerRadius: CGFloat = 0
+    func makeUIView(context: UIViewRepresentableContext<Self>) -> UIVisualEffectView { UIVisualEffectView() }
+    func updateUIView(_ uiView: UIVisualEffectView, context: UIViewRepresentableContext<Self>) {
+        uiView.effect = effect
+        uiView.layer.cornerRadius = cornerRadius
+        uiView.clipsToBounds = true
+        uiView.layer.borderWidth = 1
+        uiView.layer.borderColor = CGColor(red: 1, green: 1, blue: 1, alpha: 1)
+    }
+}
